5023a4462487e3c187a85e171f1fa18372abf38c
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-13 18:25:18 -0300

    Adicionando os arquivos da atividade finalizada

---
+++ val_aleatorios/valores_aleat_rios1.ino
@@ -0,0 +1,30 @@
+// C++ code
+//
+int ledorange = 13;//Declaração de variaveis para atribuição da indentificação dos pinos, referente aos led's.
+int ledblue = 12;
+int ledgreen = 11;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(ledorange, OUTPUT);//Declaração de como será utilização dos pinos.
+  pinMode(ledblue, OUTPUT);
+  pinMode(ledgreen, OUTPUT);
+}
+
+void loop()
+{
+  //Através da onda PWM, acionada pela função (analogWrite (pino, valor)).  
+  //Iremos variar a intesidade do brilho dos led's com a utilização da onda quadrada com o duty cycle (ciclo de trabalho).
+  //Essa variação da onda em duty cicle desta funçao(analogWrite), vai de 0 (sempre desligado) para 255(sempre ligado).
+  //Para isso utilizaremos auxilio da função random(), para fazer essa variação de intesidade de forma randômica ou aleatória.
+  
+  Serial.println();
+  analogWrite(ledorange, random(155));
+  analogWrite(ledblue, random(155));
+  analogWrite(ledgreen, random(155));         
+  delay(100);
+  
+  
+  
+}
