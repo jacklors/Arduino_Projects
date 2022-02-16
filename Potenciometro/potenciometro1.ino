7fe5e8fe31384ec9f7fb062b3fc4bab680e8b300
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-16 14:17:23 -0300

    Update potenciometro1.ino

--- Potenciometro/potenciometro1.ino
+++ Potenciometro/potenciometro1.ino
@@ -1,13 +1,29 @@
 // C++ code
-//
+/* ===============================================================================================================
+  Projeto: Controle de Potenciometro
+  Utilizado: Arduino UNO, Potenciometro e Led's
+
+                         
+                          *****DESCRIÇÃO FUNCIONAMENTO*****
+                          
+  Atráves do uso de um potenciometro, iremos controlar o acionamento dos led's.
+  ===============================================================================================================*/
+
+// ========================================= Defines e Hardware =================================================
+
+// -- Variáveis e constantes --
+
 byte ledArray [] = {13,12,11,10};//Variavel responsável pelos led's, declarada em arranjo(array) sequecial.
 int ledDelay;//Variavel que vai receber o valor da variavel (pot).
 int pot = A1;//Variavel que vai armazenar sinal analogico emitido pelo potenciometro.
 
 void setup()
 {
+  // -- Serial monitor para acompanhar informações --
   Serial.begin(9600);//Para utilizar o monitor serial
   Serial.println(pot);//Utilizado para exibir pelo monitor serial o valor da variavvel (pot).
+  
+  
   for (int i = 0; i < 4; i++ ){//Estrtura para repetir a leitura de todas as saidas, acionando os led's.
   pinMode(ledArray[i], OUTPUT);//Atraves dos arranjos e com uma variavel(i), serve trafegar pelos indices acionando cada saida especifica. 
   }
