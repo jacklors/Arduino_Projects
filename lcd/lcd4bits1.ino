4452c98ef6eedf0311c29b8ec801fcaa1494aff3
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-16 14:45:50 -0300

    Update lcd4bits1.ino

--- lcd/lcd4bits1.ino
+++ lcd/lcd4bits1.ino
@@ -1,8 +1,19 @@
 // C++ code
-//
+/* ===============================================================================================================
+  Projeto: Teste de um display
+  Utilizado: Arduino UNO, Display e um potenciometro.
+
+                           *****DESCRIÇÃO FUNCIONAMENTO*****
+  Após ligado o arduino irá escrever uma mensagem no display lcd.
+  ===============================================================================================================*/
+
+// ========================================= Adicionando a biblioteca LCD  ================================================= //
+
 #include <LiquidCrystal.h>//Adicionando a biblioteca lcd 
 LiquidCrystal lcd (12,13,4,5,6,7);// Declarando a portas pin de acordo com a referencias do display lcd.
 //                (R2,E,DB4,DB5,DB6,DB7).
+
+// ================================== Variáveis e Constantes (Globais) ============================================ //
 
 int numlin = 2;//Variavel para o numero de linhas.
 int numcol = 16;//Variavel para o numero de colunas.
