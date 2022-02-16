1ea8aede8d5caaea4efc45cb1fe57dd09046c922
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-16 14:53:03 -0300

    Update valores_aleat_rios1.ino

--- val_aleatorios/valores_aleat_rios1.ino
+++ val_aleatorios/valores_aleat_rios1.ino
@@ -1,5 +1,22 @@
 // C++ code
-//
+
+/* ===============================================================================================================
+  Projeto: Controle PWM de transmissão de empilhadeira com Arduino
+  Utilizado: Arduino UNO, Sensor Sharp GP2Y1014AU0F e módulo relé 2 canais
+
+                                     *****DESCRIÇÃO FUNCIONAMENTO*****
+                                     
+  Através da onda PWM, acionada pela função (analogWrite (pino, valor)), iremos variar a intesidade do brilho dos led's 
+  com a utilização da onda quadrada com o duty cycle (ciclo de trabalho). 
+  Essa variação da onda em duty cicle desta funçao(analogWrite), vai de 0 (sempre desligado) para 255(sempre ligado), 
+  e para isso utilizaremos auxilio da função random(), para fazer essa variação de intesidade de forma randômica ou 
+  aleatória.
+  ===============================================================================================================*/
+
+
+
+// ================================== Variáveis e Constantes (Globais) ============================================ //
+
 int ledorange = 13;//Declaração de variaveis para atribuição da indentificação dos pinos, referente aos led's.
 int ledblue = 12;
 int ledgreen = 11;
