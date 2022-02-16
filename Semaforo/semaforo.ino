992ca736520d2aedcaec6933be085c8447b3df2f
Jackson Loredo <95225193+jacklors@users.noreply.github.com>
2022-02-16 14:34:23 -0300

    Update semaforo.ino

--- Semaforo/semaforo.ino
+++ Semaforo/semaforo.ino
@@ -1,12 +1,29 @@
 // C++ code
 //Pequena simulação de um semáforo.
 //Small simulation of a traffic light.
-int ledred = 13;
-int ledyellow = 12;
-int ledgreen= 8;
+/* ===============================================================================================================
+  Projeto: Controle de um semaforo.
+  Utilizado: Arduino UNO, led's e um interruptor.
+
+                           *****DESCRIÇÃO FUNCIONAMENTO*****
+  Se o interruptor estiver em posição fechada, os led's acendem de forma controlada de acordo com a configuração de
+  um semafaro, sequencialmente.
+  ===============================================================================================================*/
+
+// ========================================= Defines e Hardware ================================================= //
+
+// Entrada 
+
+#define ledred  13
+#define ledyellow  12
+#define ledgreen 8
+#define button  9 //light switch
+
+
+// -- Variáveis e constantes --
+
 int delayPeriod = 2000;//Short time, for in the yellow lights.
 int delayPeriod2 = 6000;//Long Time, for the moving cars and red lights.
-int button = 9;//light switch
 int statebutton = 0;//Variable to save the light switch state.
 void setup()
 {
